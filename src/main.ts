e1f4d9b8b44943851e2bc65d2fb4e4fca2bf3f83
KeterVM <65543524+KeterVM@users.noreply.github.com>
2025-12-27 01:35:01 +0700

    refactor: remove PiniaColada plugin registration
    - Removed PiniaColada plugin import and usage as it's been replaced by TanStack Query
    - Fixed trailing comma formatting in VueQueryPlugin configuration for consistency

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { PiniaColada } from '@pinia/colada'
 import { i18n } from './plugins/i18n'
 import { createHead } from '@unhead/vue/client'
 
@@ -14,7 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PiniaColada)
 app.use(i18n)
 app.use(createHead())
 app.use(VueQueryPlugin, {
@@ -22,9 +20,9 @@ app.use(VueQueryPlugin, {
     defaultOptions: {
       queries: {
         retry: 0, // disable retry by default
-      }
-    }
-  }
+      },
+    },
+  },
 })
 
 app.mount('#app')
